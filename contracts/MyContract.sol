2a0169ff408718999325fcb0d041c880d8bb20ca
mwihoti <danielmwihoti@gmail.com>
2024-05-16 12:59:31 +0300

    abi -decode if value no bitcoin

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -26,7 +26,25 @@ contract MyContract is zContract, OnlySystem {
             targetTokenAddress = BytesHelperLib.bytesToAddress(message, 0);
             recipientAddress = abi.encodePacked(
                 BytesHelperLib.bytesToAddress(message, 20);
-            )
+            );
+        } else {
+            (address targetToken, bytes memory recipient) = abi.decode(
+                message,
+                (address, bytes)
+            );
+            targetTokenAddress = targetToken;
+            recipientAddress = recipient;
         }
+
+        (address gasZRC20, uint256 gasFee) = IZRC20(targetTokenAddress).withdrawGasFee();
+
+        uint256 inputForGas = SwapHelperLib.swapTokensForExactTokens(
+            systemContract,
+            zrc20,
+            gasFee,
+            gasZRC20,
+            amount
+        );
+        uint256 outputAmount - SwapHelperLib.swapTokensForExactTokens ()
       
 }
