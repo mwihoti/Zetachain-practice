c843c9e25f9c22f27aee3f8ac09cc8aaaa45c9e0
mwihoti <danielmwihoti@gmail.com>
2024-05-16 12:49:52 +0300

    system contract extract message

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -7,6 +7,7 @@ import "@zetachain/toolkit/contracts/OnlySystem.sol";
 
 contract MyContract is zContract, OnlySystem {
     SystemContract public systemContract;
+    uint256 constant BITCOIN = 18332; 
 
     constructor(address systemContractAddress) {
         systemContract = SystemContract(systemContractAddress);
@@ -18,6 +19,14 @@ contract MyContract is zContract, OnlySystem {
         uint256 amount,
         bytes calldata message
     ) external virtual override onlySystem(systemContract) {
-        // TODO: implement the logic
-    }
+        address targetTokenAddress;
+        bytes memory recipientAddress;
+
+        if (context.chainId == BITCOIN) {
+            targetTokenAddress = BytesHelperLib.bytesToAddress(message, 0);
+            recipientAddress = abi.encodePacked(
+                BytesHelperLib.bytesToAddress(message, 20);
+            )
+        }
+      
 }
